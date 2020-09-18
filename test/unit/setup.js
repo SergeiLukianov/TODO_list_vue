54121539008de4b072b2a9b6f00bab10a4d17fb0
SergeiLukianov <serjluk4@gmail.com>
2020-09-18 20:04:56 +0300

    adjusted config for tests

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,3 +1,15 @@
 import Vue from 'vue'
 
 Vue.config.productionTip = false
+
+global.localStorage = {
+  setItem: jest.fn(),
+  getItem: jest.fn(),
+  removeItem: jest.fn(),
+};
+
+global.sessionStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+};
